d82065364cd91923494855be0c6b7cdcb1b4aeb4
Paul <paul@zag.net>
2018-12-21 09:24:41 -0600

    slightly fixed day 20

--- 2018/20/day20.py
+++ 2018/20/day20.py
@@ -7,16 +7,14 @@ moves = {"W": (-1, 0), "E": (1, 0), "N": (0, -1), "S": (0, 1)}
 
 def parse(expr, pos, distances, start_x, start_y):
     x, y, length = start_x, start_y, distances[(start_x, start_y)]
-    results = []
     while expr[pos] != "$" and expr[pos] != ")":
         print_expr(expr, pos)
-        print(x, y, length, results)
+        print(x, y, length)
         move = expr[pos]
         if move == "(":
-            length, pos = parse(expr, pos+1, distances, x, y)
+            pos = parse(expr, pos+1, distances, x, y)
             # can ignore displacement caused by subexpressions
         elif move == "|":
-            results.append((length, x, y))
             x, y, length = start_x, start_y, distances[(start_x, start_y)]
         else:
             dx, dy = moves[move]
@@ -28,22 +26,18 @@ def parse(expr, pos, distances, start_x, start_y):
                 length += 1
                 distances[(x, y)] = length
         pos += 1
-    results.append((length, x, y))
-    longest, _, _ = max(results)
-    # print_expr(expr, pos)
-    # print(results, "returning", longest)
-    return longest, pos
+    print_expr(expr, pos)
+    return pos
 
 
-with open("day20input.txt") as file:
+with open("day20test5.txt") as file:
     expr = file.readline().strip()
 
 start_x, start_y = 200, 200
 distances = {(start_x, start_y): 0}
-longest, pos = parse(expr, 1, distances, start_x, start_y)
+pos = parse(expr, 1, distances, start_x, start_y)
 if pos != len(expr) - 1:
     print("didn't reach end", pos, len(expr))
 
-print(longest)
-print(len(distances))
+print(max(distances.values()))
 print(sum(1 for range in distances.values() if range >= 1000))
