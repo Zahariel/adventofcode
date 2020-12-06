db8037caada76d021690f9b9618862340bb5a9d1
Paul Zagieboylo <paul@zag.net>
2020-12-05 23:13:47 -0600

    day 6, took me longer than it should have because I rewrote it more elegant halfway through :/

---
+++ y2020/d06/day6.py
@@ -0,0 +1,22 @@
+import functools
+import operator
+
+def parse(line):
+    return set(line)
+
+with open("input.txt") as f:
+    lines = [parse(line.strip()) for line in f]
+
+groups = []
+acc = []
+for person in lines:
+    if len(person) == 0:
+        groups.append(acc)
+        acc = []
+    else:
+        acc.append(person)
+groups.append(acc)
+
+print(sum(len(functools.reduce(operator.or_, group)) for group in groups))
+
+print(sum(len(functools.reduce(operator.and_, group, set("qwertyuiopasdfghjklzxcvbnm"))) for group in groups))
