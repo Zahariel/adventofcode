f40e558a5cd6b0e7810cd66a196a7459bd2cc09e
Paul Zagieboylo <paul@zag.net>
2023-12-01 23:19:02 -0600

    day 2. parsing took just a shamefully long time. i really need to git gud at parsing

---
+++ y2023/d02/day2.py
@@ -0,0 +1,36 @@
+import re
+from collections import defaultdict
+
+def parse(line):
+    line = line.split(": ")[1]
+    return [[re.match(r"(\d+) (.+)", color).groups() for color in draw.split(", ")] for draw in line.split("; ")]
+
+
+with open("input.txt") as file:
+    lines = [parse(line.rstrip()) for line in file]
+
+MAXIMA = {
+    'red': 12,
+    'green': 13,
+    'blue': 14,
+}
+
+def check_game(game):
+    for draw in game:
+        for amt, color in draw:
+            if int(amt) > MAXIMA[color]:
+                return False
+    return True
+
+print(sum(n+1 for n, game in enumerate(lines) if check_game(game)))
+
+
+def power_game(game):
+    requirements = defaultdict(int)
+    for draw in game:
+        for amt, color in draw:
+            if requirements[color] < int(amt):
+                requirements[color] = int(amt)
+    return requirements['red'] * requirements['green'] * requirements['blue']
+
+print(sum(power_game(game) for game in lines))
