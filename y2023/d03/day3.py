858849ed6672054a79adb9818ad048f21d065599
Paul Zagieboylo <paul@zag.net>
2023-12-02 23:37:22 -0600

    day 3. originally wrote all the helpers recursively, for some reason. iterative solutions aren't any harder!

---
+++ y2023/d03/day3.py
@@ -0,0 +1,56 @@
+def parse(line):
+    return [c for c in line]
+
+with open("input.txt") as file:
+    lines: list[list[str]] = [parse(line.rstrip()) for line in file]
+
+def in_bounds(r, c):
+    return 0 <= r < len(lines) and 0 <= c < len(lines[r])
+
+def is_symbol(r, c):
+    return in_bounds(r, c) and lines[r][c] != '.' and not lines[r][c].isdigit()
+
+def is_digit(r, c):
+    return in_bounds(r, c) and lines[r][c].isdigit()
+
+def adj_symbol(r, c):
+    return any(is_symbol(r2, c2) for r2 in [r-1, r, r+1] for c2 in [c-1, c, c+1])
+
+def find_adj_symbol(r, c):
+    while is_digit(r, c):
+        if adj_symbol(r, c): return True
+        c += 1
+    return False
+
+def read_part_number(r, c):
+    answer = 0
+    while is_digit(r, c):
+        answer *= 10
+        answer += int(lines[r][c])
+        c += 1
+    return answer
+
+def is_part_number(r, c):
+    if not is_digit(r, c): return 0
+    if is_digit(r, c-1): return 0
+    if not find_adj_symbol(r, c): return 0
+    return read_part_number(r, c)
+
+print(sum(is_part_number(r, c) for r, line in enumerate(lines) for c in range(len(line))))
+
+
+def read_any_part_number(r, c):
+    if not is_digit(r, c): return None
+    while is_digit(r, c-1):
+        c -= 1
+    return read_part_number(r, c), r, c
+
+def is_gear(r, c):
+    if lines[r][c] != '*': return 0
+    numbers = list({read_any_part_number(r2, c2) for r2 in [r-1, r, r+1] for c2 in [c-1, c, c+1]})
+    numbers.remove(None)
+    if len(numbers) != 2: return 0
+    return numbers[0][0] * numbers[1][0]
+
+
+print(sum(is_gear(r, c) for r, line in enumerate(lines) for c in range(len(line))))
