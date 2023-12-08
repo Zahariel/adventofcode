a9bea5e9e2522810fade7acfb8519b50e02ff4b4
Paul Zagieboylo <paul@zag.net>
2023-12-07 23:49:59 -0600

    day 8 again, an actually correct solution that solves the CRT problem properly and doesn't rely on the fact that the transition from the final node is always directly back to the 2nd node

--- utils.py
+++ utils.py
@@ -1,2 +1,14 @@
+import math
+
 def manhattan(coords1, coords2):
     return sum(abs(x - y) for (x, y) in zip(coords1, coords2))
+
+
+def chinese_remainder(equations):
+    lcm = math.lcm(*(modulus for _, modulus in equations))
+
+
+    inverses = [pow(modulus, -1, lcm//modulus) for _, modulus in equations]
+
+    return sum(residue * inv * (lcm // modulus) for (residue, modulus), inv in zip(equations, inverses)) % lcm, lcm
+
--- y2023/d08/day8.py
+++ y2023/d08/day8.py
@@ -3,6 +3,7 @@ import math
 from parsy import regex, seq, string
 
 from parsing import split_on_blank
+from utils import chinese_remainder
 
 def parse(line):
     node = regex(r"...")
@@ -35,3 +36,27 @@ starts = [node for node in node_map if node[-1] == 'A']
 
 times = [find_time(start, lambda n: n[-1] == 'Z') for start in starts]
 print(math.lcm(*times))
+
+# an actually correct solution to part 2, because the one above is completely wrong, despite getting the right answer
+# because of crocked inputs
+def find_loop(start, target_fn):
+    current = start
+    steps = 0
+    seen = {(start, 0)}
+    goal_time = None
+    while True:
+        current = node_map[current][instructions[steps % len(instructions)]]
+        steps += 1
+        if target_fn(current):
+            goal_time = steps
+        if (current, steps % len(instructions)) in seen:
+            break
+        else:
+            seen.add((current, steps % len(instructions)))
+
+    return goal_time, steps - (steps % len(instructions))
+
+cycles = [find_loop(start, lambda n: n[-1] == 'Z') for start in starts]
+
+answer, modulus = chinese_remainder(cycles)
+print((answer - 1) % modulus + 1)
