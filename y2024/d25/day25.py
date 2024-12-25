02329b2f71a96172e2e97279544e84888e69ad1e
Paul Zagieboylo <paul@zag.net>
2024-12-24 23:18:43 -0600

    i eventually remembered that zip(*matrix) is the same as transpose(matrix)

--- y2024/d25/day25.py
+++ y2024/d25/day25.py
@@ -1,18 +1,14 @@
 from parsing import split_on_blank
 
 
-def parse(line):
-    return line
-
-
 with (open("input.txt") as f):
-    schematics = list(split_on_blank(f, parse))
+    schematics = list(split_on_blank(f))
 
 locks = []
 keys = []
 
 for schematic in schematics:
-    pins = [sum(1 for line in schematic if line[p] == "#") for p in range(len(schematic[0]))]
+    pins = [sum(1 for x in pin if x == "#") for pin in zip(*schematic)]
     if schematic[0][0] == "#":
         locks.append(tuple(pins))
     else:
