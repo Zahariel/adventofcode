bf43a9250d1e90b6e5503ee87b532cc00fd88bda
Paul Zagieboylo <paul@zag.net>
2025-12-01 17:17:11 -0600

    day 1, bonus solution based on alex's

--- y2025/d01/day1.py
+++ y2025/d01/day1.py
@@ -41,3 +41,23 @@ for dir, dist in lines:
         password += rots
 
 print(password)
+
+# bonus kind of silly portion-based solution, loosely based on Alex's
+# using portion is tantamount to obliterating this problem with a plasma cannon, but it works
+import portion as P
+dial = 50
+password = 0
+for dir, dist in lines:
+    rots, dist = divmod(dist, 100)
+    password += rots
+    if dir == "L":
+        clicks = P.closedopen(dial-dist, dial)
+        dial -= dist
+    else:
+        clicks = P.openclosed(dial, dial+dist)
+        dial += dist
+    dial %= 100
+    if 0 in clicks or 100 in clicks:
+        password += 1
+
+print(password)
