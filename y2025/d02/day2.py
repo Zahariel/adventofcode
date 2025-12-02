1a2042c0f576a45a38900cc61ba3c93b0921f72a
Paul Zagieboylo <paul@zag.net>
2025-12-01 23:24:42 -0600

    day 2, i'm sure there's a clever way to do this but the input just wasn't very big

---
+++ y2025/d02/day2.py
@@ -0,0 +1,34 @@
+from parsy import seq, string
+
+from parsing import number
+
+
+def parse(line):
+    rng = seq(number, string("-") >> number).map(tuple)
+    return rng.sep_by(string(",")).parse(line)
+
+with open("input.txt") as f:
+    ranges = parse(f.readline().rstrip())
+
+# there's probably a better way to do this but I only have about 3M numbers to check,
+# it's easy enough to just check them all one by one
+
+def validate_id(id):
+    idstr = str(id)
+    if len(idstr) % 2 == 1: return True
+    left = idstr[:len(idstr)//2]
+    right = idstr[len(idstr)//2:]
+    if left == right: return False
+    return True
+
+print(sum(id for left, right in ranges for id in range(left, right + 1) if not validate_id(id)))
+
+def validate_id2(id):
+    idstr = str(id)
+    for part_size in range(1, len(idstr)//2 + 1):
+        if len(idstr) % part_size != 0: continue
+        parts = [idstr[idx:idx+part_size] for idx in range(0, len(idstr), part_size)]
+        if all(part == parts[0] for part in parts[1:]): return False
+    return True
+
+print(sum(id for left, right in ranges for id in range(left, right+1) if not validate_id2(id)))
